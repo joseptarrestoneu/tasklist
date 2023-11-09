718989498b9189957cd742325e0ec4bc28cb765f
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-11-09 18:48:49 +0100

    Update onboarding

--- assets/js/onboarding.js
+++ assets/js/onboarding.js
@@ -16,38 +16,34 @@ window.addEventListener("load", () => {
         let userName = event.target.parentNode.firstElementChild.value;
         let userUser = event.target.parentNode.firstElementChild.nextElementSibling.value;
         let userPassword = event.target.parentNode.firstElementChild.nextElementSibling.nextElementSibling.value;
-        
-        console.log(userName);
-        console.log(userUser);
-        console.log(userPassword);
-        
+        postUser()
     })
 
-    const postUser = () => {
-
-        let userName = document.forms["signingForm"]["name"].value;
-        let userUser = document.forms["signingForm"]["user"].value;;
-        let userPassword = document.forms["signingForm"]["password"].value;;
-    
-        const update = {
-            userName: userName,
-            userUser: userUser,
-            userPassword: userPassword,
-        };
-
-        console.log(update);
-        
-    
-        const options = {
-            method: 'POST',
-            headers: 
-                {
-                    'Content-Type': 'application/json',
-                },
-            body: JSON.stringify(update),
-        };
-    
-        fetch(`http://localhost:3001/api/users/`, options)
-    }
-
 })
+
+const postUser = () => {
+
+    let userName = document.forms["signingForm"]["name"].value;
+    let userUser = document.forms["signingForm"]["user"].value;;
+    let userPassword = document.forms["signingForm"]["password"].value;;
+
+    const update = {
+        userName: userName,
+        userUser: userUser,
+        userPassword: userPassword,
+    };
+
+    console.log(update);
+    
+
+    const options = {
+        method: 'POST',
+        headers: 
+            {
+                'Content-Type': 'application/json',
+            },
+        body: JSON.stringify(update),
+    };
+
+    fetch(`http://localhost:3001/api/users/`, options)
+}
